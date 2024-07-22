c7bfe15442908f57475c0363547cb175851eed41
aymensellaouti <aymen.sellaouti@gmail.com>
2024-07-22 14:14:39 +0100

    fin sprint rotating card

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rotating-card',
   templateUrl: './rotating-card.component.html',
-  styleUrls: ['./rotating-card.component.css']
+  styleUrls: ['./rotating-card.component.css'],
 })
 export class RotatingCardComponent {
-
+  name = 'sellaouti';
+  firstname = 'aymen';
+  job = 'formateur';
+  path = 'rotating_card_profile2.png';
 }
