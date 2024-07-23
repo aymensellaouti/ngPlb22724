a2da83f9b9df01c418688c42496e8467bad5df30
aymensellaouti <aymen.sellaouti@gmail.com>
2024-07-23 14:00:00 +0100

    add Todo for cvService

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -13,7 +13,8 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class CvComponent {
   today = new Date();
-  // Crée la liste des cvs
+  // Todo : Migrer la création de la liste vers un cvService
+  //Crée la liste des cvs
   cvs: Cv[] = [
     new Cv(1, 'lepauvre', 'Martial', 'Dev', '123', 22, '    '),
     new Cv(2, 'codet', 'Marie', 'Dev', '1234', 20, 'rotating_card_profile.png'),
